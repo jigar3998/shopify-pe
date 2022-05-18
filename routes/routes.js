605736347821d5b301497caf8cfc37c9d9cb3375
jigar3998 <jigarmehta3998@gmail.com>
2022-05-17 20:47:30 -0400

    Add Product Feature added

--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,10 @@ router.get('/',(req, res) => {
     res.render("index", {title: "Home Page"})
 })
 
+router.get('/add',(req, res) => {
+    res.render("add_products", {title: "Add Products"})
+})
+
+
 module.exports = router
 
