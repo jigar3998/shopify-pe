0475d5567f116814e10d1f4db5e280978cb39d7d
jigar3998 <jigarmehta3998@gmail.com>
2022-05-17 20:10:28 -0400

    Initialized the project

---
+++ main.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const { route } = require('./routes/routes');
+
+const app = express();
+const PORT = process.env.PORT || 4000
+
+mongoose.connect(process.env.DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+
+app.use(session({
+    secret:"My secret key",
+    saveUninitialized:true,
+    resave:false
+}))
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message
+    delete req.session.message
+    next()
+})
+
+app.set("view engine","ejs")
+
+app.use("",require("./routes/routes"))
+
+app.listen(PORT,()=>{
+    console.log(`Server started at Port: ${PORT}`)
+})
