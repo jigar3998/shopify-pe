6d474fcfae250416257d777df2cefd6b5489cb1b
jigar3998 <jigarmehta3998@gmail.com>
2022-05-18 01:19:48 -0400

    Adding form data to database

--- models/products.js
+++ models/products.js
@@ -3,27 +3,18 @@ const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema({
     name: {
         type: 'string',
-        required: true
     },
     city: {
         type: 'string',
-        required: true
     },
     description: {
         type: 'string',
-        required: true
     },
     price: {
         type: 'string',
-        required: true
-    },
-    weather: {
-        type: 'string',
-        required: true
     },
     image: {
         type: 'string',
-        required: true
     },
     created: {
         type: Date,
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,49 @@
 const express = require('express')
+require('dotenv').config();
 const router = express.Router()
+const Product = require('../models/products')
+const multer = require("multer")
+
+let city;
+
+var storage = multer.diskStorage({
+    destination: function(req, file,callback){
+        callback(null,"./uploads")
+    },
+    filename: function(req,file,callback){
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
+    }
+})
+
+var upload= multer({
+    storage: storage,
+}).single("image")
+
+// const getData = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.weatherApiKey}`)
+//   .then(response => response.json)
+//   .then(jsonObject => console.log(jsonObject))
+
+router.post('/add', upload, (req, res)=>{
+    var city = req.body.city
+    const product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        city: city,
+        image: req.file.filename
+    })
+    product.save((err)=>{
+        if(err){
+            res.json({message: err.message, type: 'danger'});
+        } else{
+            req.session.message = {
+                type: 'success',
+                message:"Product added successfully"
+            }
+            res.redirect('/')
+        }
+    })
+})
 
 router.get('/',(req, res) => {
     res.render("index", {title: "Home Page"})
