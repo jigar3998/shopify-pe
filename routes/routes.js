87237284de3340377acd7a19846d3c4003b22146
jigar3998 <jigarmehta3998@gmail.com>
2022-05-18 03:05:54 -0400

    Completed the Edit and Delete functionality for product

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 const router = express.Router()
 const Product = require('../models/products')
 const multer = require("multer")
-
-let city;
+const fs = require('fs')
 
 var storage = multer.diskStorage({
     destination: function(req, file,callback){
@@ -62,6 +61,77 @@ router.get('/add',(req, res) => {
     res.render("add_products", {title: "Add Products"})
 })
 
+router.get('/edit/:id',(req, res) => {
+    let id = req.params.id
+    Product.findById(id,(err,product)=>{
+        if(err){
+            res.redirect('/')
+        } else{
+            if(product==null){
+                res.redirect('/')
+            }else{
+                res.render("edit_products",{title: "Edit Product", product:product})
+            }
+        }
+    })
+})
+
+router.post('/update/:id', upload, (req, res)=>{
+    let id = req.params.id
+    let newImg = ""
+
+    if(req.file){
+        newImg = req.file.filename
+        try{
+            fs.unlinkSync("./uploads/"+ req.body.old_image)
+        } catch(e){
+            console.error
+        }
+    }else{
+        newImg = req.body.old_image
+    }
+
+    Product.findByIdAndUpdate(id,{
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        city: req.body.city,
+        image: newImg
+    },(err,result)=>{
+        if(err){
+            res.json({message:err.message, type:'danger'})
+        }else {
+            req.session.message = {
+                type: 'success',
+                message: 'Product updated successfully'
+            }
+            res.redirect('/')
+        }
+    })
+})
+
+router.get('/delete/:id',(req, res)=>{
+    let id = req.params.id
+    Product.findByIdAndRemove(id,(err, result)=>{
+        if(result.image!=""){
+            try{
+                fs.unlinkSync('/uploads/'+result.image)
+            }catch(err){
+                console.log(err)
+            }
+        }
+
+        if(err){
+            res.json({message:err.message, type:'danger'})
+        }else {
+            req.session.message = {
+                type: 'info',
+                message: 'Product deleted successfully'
+            }
+            res.redirect('/')
+        }
+    })
+})
 
 module.exports = router
 
